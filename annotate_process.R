f8f5e2bbd1f6dd42af07572d7567d4f208356046
Winston <wtimp@jhu.edu>
2017-06-07 15:11:36 -0400

    First assembly qc

--- annotate_process.R
+++ annotate_process.R
@@ -1,112 +1,126 @@
+##Ok - this code is to do a quick annotation of k-pneumo like assemblies output from canu
+##Example use
+##Rscript ~/Code/timp_nanopore/oxford/fq_check.R $PWD/170519_cdna_cel_smu1_111.csv.gz ~/Dropbox/Data/Nanopore/170528_worm/ 170519_cdna
+
 library(tidyverse)
-library(googlesheets)
 
-workdir="/atium/Data/Nanopore/Analysis/170104_reassemble/annot/"
-workdir2="/atium/Data/Nanopore/Analysis/170330_oxa/annot/"
-outdir="~/Dropbox/Data/Nanopore/170402_reassemble"
+codedir="~/Code/timp_nanopore/"
 
-gs_auth(token = "~/googlesheets_token.rds")
 
-fullsheet=gs_url("https://docs.google.com/spreadsheets/d/1_WT3RQSVGvR97-asIHtIy0WWg49rAFiWWQe9g8BWNiQ/edit?usp=sharing")
-dataloc=gs_read(fullsheet, ws="KPneumo0402") %>%
-    filter(Sequenced=="both")
+##Default input variable
+workdir="/atium/Data/Nanopore/Analysis/170512_2897_KLPN/"
+assfile=file.path(workdir, "canu_assembly", "170512_2897_KLPN.contigs.fasta")
+prefix="170512_2897_KLPN"
+outdir="~/Dropbox/Data/CPOWGS/170512_2897_KLPN"
 
+
+##This pulls in the command line stuff
+args=(commandArgs(TRUE))
+
+##Check if there are any variables, and if not, keep default
+if(length(args)==0) {
+    print("No arguments.")  
+}else{
+    csvfile=args[1]
+    plotdir=args[2]
+    modif=args[3]
+}
+
+##print(args)
+
+##make annotation dir
+if (!(dir.exists(file.path(outdir, "annot")))) {
+    dir.create(file.path(outdir, "annot"))
+}
+
+
+##Setup blast/card info
 blast.cols=c("query.id", "subject.id", "per.identity", "align.len", "mismatch", "gap.opens", "q.start", "q.end", "s.start", "s.end", "evalue", "bit.score")
-
 card.dir="/atium/Data/Reference/CARD/"
-
 card.tab=read_csv(file.path(card.dir, "aro.csv")) %>%
     mutate(Description=gsub('\n', '', Description))
 
 
-##Fix card tab - clean to remove \ns
+setwd(workdir)
 
-##Fix to make it one hit only for card blast?
+##Run Prokka
+system(paste0("~/Code/prokka/bin/prokka --outdir ", workdir, "/", prefix, "_genus --genus Klebsiella --usegenus --prefix ",
+              prefix, "_genus ", assfile))
 
-setwd(workdir2)
+file.copy(file.path(workdir, paste0(prefix, "_genus"), paste0(prefix, "_genus.gff")),
+          file.path(outdir, "annot", paste0(prefix, "_genus.gff")))
 
-##Load CSV with sample info
+##Run RGI (CARD)
+system(paste0("python ~/Code/rgi_card/rgi.py -t contig -i ", assfile, " -n 8 -o ", prefix, ".rgicard"))
+system(paste0("python ~/Code/rgi_card/convertJsonToTSV.py -i ", prefix, ".rgicard.json -o ", prefix, ".rgicard"))
 
-for (i in 1:dim(dataloc)[1]) {
+rgi=read_tsv(file.path(workdir, paste0(prefix, ".rgicard.txt"))) %>%
+    mutate(clabel=gsub('_[0-9]*$', '', ORF_ID)) %>%
+    mutate(attrib=paste0("Name=",ARO_name, ";gene=", ARO_name, ";ARO_category=", AR0_category, ";cutoff=", CUT_OFF))
     
-    samp=dataloc$trish.id[i]
+rgi.gff=rgi %>%
+    mutate(source="rgicard", feature="CDS", frame="0") %>%
+    select(seqname=clabel, source=source, feature=feature, start=START,
+           end=STOP, score=Best_Identities, strand=ORIENTATION,
+           frame=frame, attribute=attrib) 
     
-    ##Run Prokka
-    system(paste0("~/Code/prokka/bin/prokka --outdir ", workdir2, "/", samp, "_genus --genus Klebsiella --usegenus --prefix ", samp, "_genus ", dataloc$pilon2[i]))
-    
-    file.copy(file.path(workdir2, paste0(samp, "_genus"), paste0(samp, "_genus.gff")), file.path(outdir, "annot", paste0(samp, "_genus.gff")))
-    
-    ##Run RGI (CARD)
-    system(paste0("python ~/Code/rgi_card/rgi.py -t contig -i ", dataloc$pilon2[i], " -n 8 -o ", samp, ".rgicard"))
-    system(paste0("python ~/Code/rgi_card/convertJsonToTSV.py -i ", samp, ".rgicard.json -o ", samp, ".rgicard"))
-    
-    rgi=read_tsv(file.path(workdir2, paste0(samp, ".rgicard.txt"))) %>%
-        mutate(clabel=gsub('_[0-9]*$', '', ORF_ID)) %>%
-        mutate(attrib=paste0("Name=",ARO_name, ";gene=", ARO_name, ";ARO_category=", AR0_category, ";cutoff=", CUT_OFF))
-    
-    rgi.gff=rgi %>%
-        mutate(source="rgicard", feature="CDS", frame="0") %>%
-        select(seqname=clabel, source=source, feature=feature, start=START,
-               end=STOP, score=Best_Identities, strand=ORIENTATION,
-               frame=frame, attribute=attrib) 
-    
-    write_tsv(rgi.gff, file.path(outdir, "annot", paste0(samp, ".rgicard.gff")), col_names=F)
+write_tsv(rgi.gff, file.path(outdir, "annot", paste0(prefix, ".rgicard.gff")), col_names=F)
     
     
-    ##Run CARD Blast
-    system(paste0("blastn -db /atium/Data/Reference/CARD/CARDblast -query ", dataloc$pilon2[i],
-                  " -evalue 1e-50 -outfmt 6 -best_hit_overhang .1 -best_hit_score_edge 1e-5 >", workdir2, "/", samp, ".cardblast.tsv"))
+##Run CARD Blast
+system(paste0("blastn -db /atium/Data/Reference/CARD/CARDblast -query ", assfile,
+              " -evalue 1e-50 -outfmt 6 -best_hit_overhang .1 -best_hit_score_edge 1e-5 >", workdir, "/", prefix, ".cardblast.tsv"))
     
-    card.res=read_tsv(file.path(workdir2, paste0(samp, ".cardblast.tsv")), col_names=blast.cols) %>%
-        mutate(aro=sapply(strsplit(subject.id, split="\\|"), function(x) {x[5]})) %>%
-        mutate(aro.idx=pmatch(aro, card.tab$Accession, duplicates.ok=TRUE)) %>%
-        mutate(aro.name=card.tab$Name[aro.idx]) %>%
-        mutate(aro.desc=card.tab$Description[aro.idx])   
+card.res=read_tsv(file.path(workdir, paste0(prefix, ".cardblast.tsv")), col_names=blast.cols) %>%
+    mutate(aro=sapply(strsplit(subject.id, split="\\|"), function(x) {x[5]})) %>%
+    mutate(aro.idx=pmatch(aro, card.tab$Accession, duplicates.ok=TRUE)) %>%
+    mutate(aro.name=card.tab$Name[aro.idx]) %>%
+    mutate(aro.desc=card.tab$Description[aro.idx])   
     
-    card.gff=card.res %>%
-        mutate(source="cardblast", feature="CDS", frame="0") %>%
-        mutate(strand=ifelse(s.end>s.start, yes="+", no="-")) %>%
-        mutate(attrib=paste0("Name=", aro.name, ";gene=", aro.name, ";per.identity=", per.identity, ";bit.score=", bit.score,";evalue=",evalue, ";ARO=",aro, ";Description=",aro.desc)) %>%
-        select(seqname=query.id, source=source, feature=feature, start=q.start, end=q.end, score=bit.score, strand=strand, frame=frame, attribute=attrib)
+card.gff=card.res %>%
+    mutate(source="cardblast", feature="CDS", frame="0") %>%
+    mutate(strand=ifelse(s.end>s.start, yes="+", no="-")) %>%
+    mutate(attrib=paste0("Name=", aro.name, ";gene=", aro.name, ";per.identity=", per.identity, ";bit.score=", bit.score,";evalue=",evalue, ";ARO=",aro, ";Description=",aro.desc)) %>%
+    select(seqname=query.id, source=source, feature=feature, start=q.start, end=q.end, score=bit.score, strand=strand, frame=frame, attribute=attrib)
 
-    write_tsv(card.gff, file.path(outdir, "annot", paste0(samp, ".cardblast.gff")), col_names=F)
+write_tsv(card.gff, file.path(outdir, "annot", paste0(prefix, ".cardblast.gff")), col_names=F)
     
     
     
-    ##Look for specific gene mutations
-    mutgenedb="/atium/Data/Nanopore/Analysis/161223_kpneumo/genedb/mutdbase"
-    system(paste0("blastn -db ", mutgenedb, " -query ", dataloc$pilon2[i], " -outfmt 6 >", workdir2, samp, ".mutblast.tsv"))
-    
-    mut.res=read_tsv(file.path(workdir2, paste0(samp, ".mutblast.tsv")), col_names=blast.cols)
-    
-    mut.gff=mut.res %>%
-        mutate(source="mutblast", feature="CDS", frame="0") %>%
-        mutate(strand=ifelse(s.end>s.start, yes="+", no="-")) %>%
-        mutate(attrib=paste0("Name=", subject.id, ";gene=", subject.id, ";per.identity=", per.identity, ";bit.score=", bit.score,";evalue=",evalue)) %>%
-        select(seqname=query.id, source=source, feature=feature, start=q.start, end=q.end, score=bit.score, strand=strand, frame=frame, attribute=attrib)
-    
-    write_tsv(mut.gff, file.path(outdir, "annot", paste0(samp, ".mutblast.gff")), col_names=F)
-    
-    
-    ##look for specific is recog sequences
-    isedb="/atium/Data/Nanopore/Analysis/161223_kpneumo/genedb/isecp1blast"
-    system(paste0("blastn -task blastn-short -db ", isedb, " -query ", dataloc$pilon2[i], " -outfmt 6 >", workdir2, samp, ".iseblast.tsv"))
-    
-    ise.res=read_tsv(file.path(workdir2, paste0(samp, ".iseblast.tsv")), col_names=blast.cols)
-    
-    ise.gff=ise.res %>%
-        mutate(source="iseblast", feature="Mobile_element", frame="0") %>%
-        mutate(strand=ifelse(s.end>s.start, yes="+", no="-")) %>%
-        mutate(attrib=paste0("Name=", subject.id, ";gene=", subject.id, ";per.identity=", per.identity, ";bit.score=", bit.score,";evalue=",evalue)) %>%
-        select(seqname=query.id, source=source, feature=feature, start=q.start, end=q.end, score=bit.score, strand=strand, frame=frame, attribute=attrib)
-    
-    write_tsv(ise.gff, file.path(outdir, "annot", paste0(samp, ".iseblast.gff")), col_names=F)
+##Look for specific gene mutations
+mutgenedb="/atium/Data/Nanopore/Analysis/161223_kpneumo/genedb/mutdbase"
+system(paste0("blastn -db ", mutgenedb, " -query ", assfile, " -outfmt 6 >", workdir, prefix, ".mutblast.tsv"))
+
+mut.res=read_tsv(file.path(workdir, paste0(prefix, ".mutblast.tsv")), col_names=blast.cols)
+
+mut.gff=mut.res %>%
+    mutate(source="mutblast", feature="CDS", frame="0") %>%
+    mutate(strand=ifelse(s.end>s.start, yes="+", no="-")) %>%
+    mutate(attrib=paste0("Name=", subject.id, ";gene=", subject.id, ";per.identity=", per.identity, ";bit.score=", bit.score,";evalue=",evalue)) %>%
+    select(seqname=query.id, source=source, feature=feature, start=q.start, end=q.end, score=bit.score, strand=strand, frame=frame, attribute=attrib)
+
+write_tsv(mut.gff, file.path(outdir, "annot", paste0(prefix, ".mutblast.gff")), col_names=F)
 
 
-    ##MLST typing
-    system(paste0("~/Code/mlst/bin/mlst ", dataloc$pilon2[i], " >", workdir2, samp, ".mlst.tsv"))
+##look for specific is recog sequences
+isedb="/atium/Data/Nanopore/Analysis/161223_kpneumo/genedb/isecp1blast"
+system(paste0("blastn -task blastn-short -db ", isedb, " -query ", assfile, " -outfmt 6 >", workdir, prefix, ".iseblast.tsv"))
+
+ise.res=read_tsv(file.path(workdir, paste0(prefix, ".iseblast.tsv")), col_names=blast.cols)
+
+ise.gff=ise.res %>%
+    mutate(source="iseblast", feature="Mobile_element", frame="0") %>%
+    mutate(strand=ifelse(s.end>s.start, yes="+", no="-")) %>%
+    mutate(attrib=paste0("Name=", subject.id, ";gene=", subject.id, ";per.identity=", per.identity, ";bit.score=", bit.score,";evalue=",evalue)) %>%
+    select(seqname=query.id, source=source, feature=feature, start=q.start, end=q.end, score=bit.score, strand=strand, frame=frame, attribute=attrib)
+
+write_tsv(ise.gff, file.path(outdir, "annot", paste0(prefix, ".iseblast.gff")), col_names=F)
+
+
+##MLST typing
+system(paste0("~/Code/mlst/bin/mlst ", assfile, " >", workdir, prefix, ".mlst.tsv"))
     
-}
+
 
 
 
--- assembly_compare.sh
+++ assembly_compare.sh
@@ -1,42 +1,36 @@
 #!/bin/bash
 
+assfile=$1
+
+genome=$2
+
+case $genome in
+    kpn )
+	echo "kpn"
+	reffasta=/mithril/Data/NGS/Reference/kpneumo/NC_016845.fasta
+	;;
+esac
+
+outdir=$3/wga
+
+prefix=$4
+
+mkdir -p ${outdir}
+
+
 ##Ok - to compare assemblies/align with nucmer and make dotplot?
 
-for i in 1 2 9 12
-do
-    qseq=canu/${i}/${i}.nanopore.contigs.fasta
-    
-    ~/Code/mummer-4.0.0beta/nucmer -p wga/${i}.canu NC_016845.fasta ${qseq}
-    ~/Code/mummer-4.0.0beta/mummerplot --png -p wga/${i}.layout.canu wga/${i}.canu.delta -l -R NC_016845.fasta -Q ${qseq}
-    ~/Code/mummer-4.0.0beta/mummerplot --png -p wga/${i}.canu wga/${i}.canu.delta 
+~/Code/mummer-4.0.0beta/nucmer -p ${outdir}/${prefix} ${reffasta} ${assfile}
+~/Code/mummer-4.0.0beta/mummerplot --png -p ${outdir}/${prefix}.layout.canu ${outdir}/${prefix}.delta \
+				   -l -R ${reffasta} -Q ${assfile}
+~/Code/mummer-4.0.0beta/mummerplot --png -p ${outdir}/${prefix}.canu ${outdir}/${prefix}.delta 
 
-    ~/Code/mummer-4.0.0beta/mummerplot --postscript -p wga/${i}.layout.canu wga/${i}.canu.delta -l -R NC_016845.fasta -Q ${qseq}
-    ~/Code/mummer-4.0.0beta/mummerplot --postscript -p wga/${i}.canu wga/${i}.canu.delta 
+~/Code/mummer-4.0.0beta/mummerplot --postscript -p ${outdir}/${prefix}.layout.canu ${outdir}/${prefix}.delta \
+				   -l -R ${reffasta} -Q ${assfile}
+~/Code/mummer-4.0.0beta/mummerplot --postscript -p ${outdir}/${prefix}.canu ${outdir}/${prefix}.delta 
 
-    lastal -P 8 /mithril/Data/NGS/Reference/kpneumo/kpneumo ${qseq} >wga_last/${i}.canu.maf
-    last-dotplot wga_last/${i}.canu.maf wga_last/${i}.canu.pdf
-    last-dotplot wga_last/${i}.canu.maf wga_last/${i}.canu.png
+lastal -P 8 /mithril/Data/NGS/Reference/kpneumo/kpneumo ${assfile} >${outdir}/${prefix}.canu.maf
+last-dotplot ${outdir}/${prefix}.canu.maf ${outdir}/${prefix}.canu.pdf
+last-dotplot ${outdir}/${prefix}.canu.maf ${outdir}/${prefix}.canu.png
 
     
-done
-
-##spades
-
-for i in 1 2 9 12
-do
-    qseq=spades/${i}/contigs.fasta
-    
-    ~/Code/mummer-4.0.0beta/nucmer -p wga/${i}.spades NC_016845.fasta ${qseq}
-    ~/Code/mummer-4.0.0beta/mummerplot --png -p wga/${i}.layout.spades wga/${i}.spades.delta -l -R NC_016845.fasta -Q ${qseq}
-    ~/Code/mummer-4.0.0beta/mummerplot --png -p wga/${i}.spades wga/${i}.spades.delta 
-
-    ~/Code/mummer-4.0.0beta/mummerplot --postscript -p wga/${i}.layout.spades wga/${i}.spades.delta -l -R NC_016845.fasta -Q ${qseq}
-    ~/Code/mummer-4.0.0beta/mummerplot --postscript -p wga/${i}.spades wga/${i}.spades.delta 
-
-    lastal -P 8 /mithril/Data/NGS/Reference/kpneumo/kpneumo ${qseq} >wga_last/${i}.spades.maf
-    last-dotplot wga_last/${i}.spades.maf wga_last/${i}.spades.pdf
-    last-dotplot wga_last/${i}.spades.maf wga_last/${i}.spades.png
-
-
-    
-done
